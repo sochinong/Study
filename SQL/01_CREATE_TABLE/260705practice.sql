2716547b5f9cc73f7417100d8ec1a5d2b9f47c61
소윤서 <a01024208430@gmail.com>
2026-07-05 15:18:59 +0900

    Create 260705practice.sql

---
+++ SQL/01_CREATE_TABLE/260705practice.sql
@@ -0,0 +1,11 @@
+CREATE TABLE product(
+    product_name VARCHAR(20),
+    cost INT,
+    make_date DATE,
+    company VARCHAR(20),
+    amount INT
+);
+
+INSERT INTO product
+VALUES
+('바나나',1500,'2021-07-01','델몬트',17);
