ead39a8e794650536530197ad66783300e67e733
소윤서 <a01024208430@gmail.com>
2026-07-06 15:03:59 +0900

    Create 260705select_from_where기본.sql
    select, from, where의 기본적인 절

---
+++ SQL/03_Select_From_Where/260705select_from_where기본.sql
@@ -0,0 +1,15 @@
+use market_db;
+select * from member;
+select mem_name from member;
+select addr, debut_date, mem_name from member;
+select addr, debut_date "데뷔 일자", mem_name from member;
+select * from member where mem_name = '블랙핑크';
+select * from member where mem_numer = 4;
+select mem_id, mem_name from member where height <= 162;
+select mem_name, height, mem_number From member where height >=165 and mem_number >6;
+select mem_name, height, mem_number From member where height >=165 or mem_number >6;
+select mem_name, height, mem_number From member where height >=163 and height <=165;
+select mem_name, height, mem_number From member where height between 163 and 165;
+select mem_name, addr from member where addr in('경기', '전남', '경남');
+select * from member where mem_name like '우%';
+select * from member where mem_name like '__핑크';
